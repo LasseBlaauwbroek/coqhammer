2a6c45cd4d6325c548dbc534297898ec76b4ba2d
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2024-11-01 23:16:48 +0100

    Sauto

--- src/plugin/hammer_main.ml
+++ src/plugin/hammer_main.ml
@@ -1085,7 +1085,7 @@ module HLearner : TacticianOnlineLearnerType = functor (TS : TacticianStructures
 
   let extra_tactic () = { confidence = 1.; focus = 0
                         ; tactic = tactic_make
-                              (get_tacexpr "Hammer.Plugin.Hammer.hammer" []) }
+                              (get_tacexpr "Hammer.Tactics.Tactics.sauto" []) }
   let empty () = ()
   let learn () _ _ _ = ()
   let predict m s =
